fa1842b6d881e44ad3abf880e172f0bb7ee641e2
Joe Rivera <dvs_joe@hotmail.com>
2023-05-11 23:14:33 -0400

    finished up mock up script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,24 @@ var apiKey = ''
 var searchForm = document.getElementById('seaarch-form');
 var seaarchInput = document.getElementById('search-input');
 var weatherInfo = document.getElementById ('weather-info');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const city = searchInput.value;
+    getWeather(city);
+  });
+  
+  function getWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const cityName = data.name;
+        weatherInfo.innerHTML = `${cityName}: ${temperature}°C, ${description}`;
+      })
+      .catch((error) => {
+        console.log(error);
+        weatherInfo.innerHTML = 'An error occurred. Please try again.';
+      });
+  }
