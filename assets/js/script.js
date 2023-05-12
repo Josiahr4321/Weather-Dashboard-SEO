42d895e2d33f050494b0f691a7b60b22008e37c5
Joe Rivera <dvs_joe@hotmail.com>
2023-05-11 23:08:11 -0400

    added script

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var apiKey = ''
+
+var searchForm = document.getElementById('seaarch-form');
+var seaarchInput = document.getElementById('search-input');
+var weatherInfo = document.getElementById ('weather-info');
