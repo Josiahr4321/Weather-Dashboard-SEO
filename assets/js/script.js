0057b940dbfe781e8297a2e1c586cfa9dd5689d9
Joe Rivera <dvs_joe@hotmail.com>
2023-05-11 23:17:11 -0400

    added basic script

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,3 @@ var searchForm = document.getElementById('seaarch-form');
 var seaarchInput = document.getElementById('search-input');
 var weatherInfo = document.getElementById ('weather-info');
 
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const city = searchInput.value;
-    getWeather(city);
-  });
-  
